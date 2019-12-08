93b12ca4019703053a45f66f8e00849cbdb8e4c4
Hamza Astid <hastid@e2r9p12.1337.ma>
2019-12-08 23:32:11 +0100

    some bugs in built cd

--- src/sh_buil_cd.c
+++ src/sh_buil_cd.c
@@ -6,7 +6,7 @@
 /*   By: hastid <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/23 16:29:59 by hastid            #+#    #+#             */
-/*   Updated: 2019/12/02 02:28:53 by hastid           ###   ########.fr       */
+/*   Updated: 2019/12/08 23:29:59 by hastid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,14 @@ static int	change_dir(char *dir, t_env **env)
 	{
 		add_elem(env, "OLDPWD", pwd);
 		ft_memdel((void **)&pwd);
-		chdir(dir);
-		if ((pwd = getcwd(0, 0)))
-		{
-			add_elem(env, "PWD", pwd);
-			ft_memdel((void **)&pwd);
-		}
+	}
+	else if ((pwd = ft_getenv(*env, "PWD")))
+		add_elem(env, "OLDPWD", pwd);
+	chdir(dir);
+	if ((pwd = getcwd(0, 0)))
+	{
+		add_elem(env, "PWD", pwd);
+		ft_memdel((void **)&pwd);
 	}
 	return (0);
 }
@@ -36,19 +38,16 @@ int			built_cd(char **args, t_env **env)
 
 	if (args[1])
 	{
+		dir = args[1];
 		if (!ft_strcmp("-", args[1]))
-		{
-			if ((dir = ft_getenv(*env, "OLDPWD")))
-				change_dir(dir, env);
-			else
-				ft_perror("env", ": Oldpwd not exists", 1);
-		}
-		else if (isdir(args[1]))
-			change_dir(args[1], env);
-		else if (!access(args[1], F_OK))
-			ft_perror(args[1], ": Not a directory", 1);
+			if (!(dir = ft_getenv(*env, "OLDPWD")))
+				return (ft_perror("env", ": Oldpwd not exists", 1));
+		if (isdir(dir))
+			change_dir(dir, env);
+		else if (!access(dir, F_OK))
+			ft_perror(dir, ": Not a directory", 1);
 		else
-			ft_perror(args[1], ": No such file or directory", 1);
+			ft_perror(dir, ": No such file or directory", 1);
 	}
 	else if ((dir = ft_getenv(*env, "HOME")))
 		change_dir(dir, env);
